76b8ce72142941ad3a1186696832a1dd139190e3
RazEbrat <raz.tekschool@gmail.com>
2023-02-25 20:04:44 -0500

    changes to file

---
+++ src/pracPackage/RandomClass.java
@@ -0,0 +1,15 @@
+package pracPackage;
+
+public class RandomClass {
+
+	public static void main(String[] args) {
+		
+		// this is comment from git push
+		
+		/*
+		 * this is test 
+		 */
+
+	}
+
+}
